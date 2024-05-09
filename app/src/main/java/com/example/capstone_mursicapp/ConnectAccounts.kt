f0e90da78a5c83a7e3654dbd275fb6f70b50fab3
amonooe <amonooe@sunypoly.edu>
2024-05-09 12:33:17 -0400

    kotlin

--- app/src/main/java/com/example/capstone_mursicapp/ConnectAccounts.kt
+++ app/src/main/java/com/example/capstone_mursicapp/ConnectAccounts.kt
@@ -1,36 +1,25 @@
-package com.example.capstone_mursicapp;
+package com.example.capstone_mursicapp
 
-import android.os.Bundle;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-
-public class ConnectAccounts extends Fragment {
-    Button spotifylogin;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+class ConnectAccounts : Fragment() {
+    var spotifylogin: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_connect_accounts, container, false);
-
-        spotifylogin = view.findViewById(R.id.spotifylogin);
-
-        spotifylogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        return view;
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_connect_accounts, container, false)
+        spotifylogin = view.findViewById(R.id.spotifylogin)
+        spotifylogin.setOnClickListener(View.OnClickListener { })
+        return view
     }
 }
